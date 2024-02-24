b06326ba37aec66c1bfa23dd5f115fe985dfb977
Aklimabegum1010 <150171856+Aklimabegum1010@users.noreply.github.com>
2024-02-24 04:33:52 -0800

    upded some js file

--- add.js
+++ add.js
@@ -32,58 +32,95 @@ for (const btn of myButtonClass) {
         const taka = event.target.parentNode.parentNode.parentNode.parentNode.childNodes[3].childNodes[5].childNodes[3].innerText;
 
 
-        // console.log(event.target.parentNode.parentNode.parentNode.parentNode.childNodes[3].childNodes[5].childNodes[3].innerText)
 
-const selectedCartContainer = document.getElementById('selected-cart-container');
+        
 
-const div = document.createElement('div');
-div.classList.add('flex');
-div.classList.add('justify-between');
+        const selectedCartContainer = document.getElementById('selected-cart-container');
 
-const p1 = document.createElement('p');
-const p2 = document.createElement('p');
-const p3 = document.createElement('p');
-p1.innerText = seat;
-p2.innerText = economoy;
-p3.innerText = taka;
-
-div.appendChild(p1);
-div.appendChild(p2);
-div.appendChild(p3);
-selectedCartContainer.appendChild(div)
+        event.target.setAttribute('disabled', false)
+event.target.parentNode.childNode
 
 
+        const firstSeatCount = getConvertedValue('add-seat');
+        if (firstSeatCount + 1 > 4) {
+            alert('limit four');
+            return;
+        }
+
+
+        const addSeat = getConvertedValue('add-seat')
+        document.getElementById('add-seat').innerText = addSeat + 1;
+
+        const seatsLeft = getConvertedValue('8-seats-left')
+        document.getElementById('8-seats-left').innerText = seatsLeft - 1;
 
 
 
+
+
+        const div = document.createElement('div');
+        div.classList.add('flex');
+        div.classList.add('justify-between');
+
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+        p1.innerText = seat;
+        p2.innerText = economoy;
+        p3.innerText = taka;
+
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        selectedCartContainer.appendChild(div)
+
+        allTotalCost(taka);
+        allGrandTotal()
 
     })
 }
 
 
+function allGrandTotal(events) {
+    const totalCost = getConvertedValue('total-cost');
+    if (events == undefined) {
+        document.getElementById('grand-total').innerText = totalCost;
+    }
+    else {
+        const couponCode = document.getElementById('coupon-code').value;
+        if (couponCode == 'NEW15') {
+            const discount = totalCost * 15 / 100;
+            const newDiscount = parseInt(discount);
+            // console.log(newDiscount);
+
+            document.getElementById('grand-total').innerText = totalCost - newDiscount;
 
 
+        }
+        else if (couponCode == 'NEW 20') {
+            const discount = totalCost * 20 / 100;
+            const newDiscount = parseInt(discount);
+
+            document.getElementById('grand-total').innerText = totalCost - newDiscount;
+        }
+
+    }
 
 
+}
 
+function allTotalCost(value) {
+    const totalCost = getConvertedValue('total-cost');
+    const sum = totalCost + parseInt(value);
+    document.getElementById('total-cost').innerText = sum;
+}
 
-// const takaSection = getConvertedValue('taka-section');
-// const seatSec =  getConvertedValue('seat-sec');
-// const classSec =  getConvertedValue('class-sec');
-// const priceSec =  getConvertedValue('price-sec');
 
 function getConvertedValue(id) {
     const price = document.getElementById(id).innerText;
     const convertPrice = parseInt(price);
     return convertPrice;
 }
-// const result = getConvertedValue();
-// console.log(result)
-
-
-
-
-
 
 
 function next() {
@@ -107,14 +144,4 @@ function next() {
 
 
 
-// function handelKeybordClick(event) {
-//     console.log(event.key)
-// }
-// document.addEventListener('click', handelKeybordClick);
-// const myButtons = document.getElementsByClassName('my-button-class');
-// for(const button of myButtons){
-//     button.addEventListener("click", function(){
-//         changeButtonColor(button)
-//     });
-// }
 
