eb94d8bd40a4235daa759f954833227d5dc6c2e2
Aklimabegum1010 <150171856+Aklimabegum1010@users.noreply.github.com>
2024-02-23 19:35:43 -0800

    added all html file

---
+++ add.js
@@ -0,0 +1,37 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+    
+
+
+
+
+function next(){
+    hideElementById('main-section');
+    showElementById('last-section')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
